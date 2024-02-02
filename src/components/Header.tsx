cbb989ab1b097b7f37b50041ab81aaa00e45f52b
k-sugawa <sugawakumiko@gmail.com>
2024-02-03 00:36:31 +0900

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,18 @@ import PATH from "../path";
 import { Link } from "react-router-dom";
 
 
-const Header= (): JSX.Element => {
+const Header = (): JSX.Element => {
   return (
     <>
-    <ul>
-      <li>
-        <Link to={PATH.INQUIRY}>お問い合わせ</Link>
-      </li>
-    </ul>
+      <header>
+        <nav>
+          <ul className="d-flex align-items-center space-between">
+            <li><Link to={PATH.INQUIRY}>会員登録</Link></li>
+            <li><Link to={PATH.INQUIRY}>お問い合わせ</Link></li>
+          </ul>
+        </nav>
+
+      </header>
     </>
   );
 };
